8a758f87b70d03cec63949eb645c3199755b635b
Poncho <alfonsoamezcua31@gmail.com>
2026-03-10 01:12:34 -0600

    backendv3

--- axf_backend/index.js
+++ axf_backend/index.js
@@ -9,7 +9,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 
 // Montar rutas
